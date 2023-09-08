9570105e05e4ceb4738997c02f646eea1c5469eb
caxtry <85320559+caxtry@users.noreply.github.com>
2023-09-08 18:47:37 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 // Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
 // Copy the middleware code as it is from the above resource
 
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
   // An array of public routes that don't require authentication.
@@ -9,6 +10,18 @@ export default authMiddleware({
 
   // An array of routes to be ignored by the authentication middleware.
   ignoredRoutes: ['/api/webhook/clerk'],
+
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    // redirect them to organization selection page
+    if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/api/webhook/clerk"){
+      const orgSelection = new URL('/api/webhook/clerk', req.url)
+      return NextResponse.redirect(orgSelection)
+    }
+  }
 });
 
 export const config = {
